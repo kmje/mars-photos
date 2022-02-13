c4ccff749b4077f54b7ee4decfba44215ae274fd
Kristina <kristina.m.jefferson@gmail.com>
2022-02-13 06:38:21 -0600

    Reorganize project file structure

--- public/client.js
+++ public/client.js
@@ -2,13 +2,14 @@ const submitSearch = document.querySelector('.submit-btn');
 const solDaySelect = document.querySelector('#sol');
 const cameraSelect = document.querySelector('#camera');
 const searchListener = submitSearch.addEventListener('click', processQuery);
-const solDay, camera;
+let solDay, camera;
 
 
 function processQuery() {
     solDay = solDaySelect.value;
     camera = cameraSelect.value;
 }
+
 
 
 
